0070030ce56c0bc0feb48a4e722907566bd1c341
Toshiki Iga <igapyon@gmail.com>
2017-08-26 22:04:36 +0900

    save

---
+++ src/main/java/blanco/sfdc/jdbc/driver/App.java
@@ -0,0 +1,13 @@
+package blanco.sfdc.jdbc.driver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
