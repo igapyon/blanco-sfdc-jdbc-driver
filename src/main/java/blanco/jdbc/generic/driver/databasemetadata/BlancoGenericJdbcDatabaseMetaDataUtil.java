8ddc6e50afebbe4ec66050e503fa03a6676eef50
Toshiki Iga <igapyon@gmail.com>
2017-09-02 21:04:02 +0900

    save

--- src/main/java/blanco/jdbc/generic/driver/databasemetadata/BlancoGenericJdbcDatabaseMetaDataUtil.java
+++ src/main/java/blanco/jdbc/generic/driver/databasemetadata/BlancoGenericJdbcDatabaseMetaDataUtil.java
@@ -3,6 +3,7 @@ package blanco.jdbc.generic.driver.databasemetadata;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 public class BlancoGenericJdbcDatabaseMetaDataUtil {
@@ -14,8 +15,21 @@ public class BlancoGenericJdbcDatabaseMetaDataUtil {
 			+ ", REMARKS VARCHAR, TYPE_CAT VARCHAR, TYPE_SCHEM VARCHAR, TYPE_NAME VARCHAR" //
 			+ ", SELF_REFERENCING_COL_NAME VARCHAR, REF_GENERATION VARCHAR DEFAULT 'USER')";
 
+	public static final String DATABASEMETADATA_COLUMNS_DDL_H2 = "CREATE TABLE GMETA_COLUMNS (" //
+			+ "TABLE_CAT VARCHAR, TABLE_SCHEM VARCHAR, TABLE_NAME VARCHAR, COLUMN_NAME VARCHAR, DATA_TYPE INTEGER" //
+			+ ", TYPE_NAME VARCHAR"
+			+ ", COLUMN_SIZE INTEGER, DECIMAL_DIGITS INTEGER, NUM_PREC_RADIX INTEGER, NULLABLE INTEGER DEFAULT "
+			+ ResultSetMetaData.columnNullableUnknown + ")";
+
 	public static void initGmetaTables(final Connection conn) throws SQLException {
-		final PreparedStatement pstmt = conn.prepareStatement(DATABASEMETADATA_TABLES_DDL_H2);
+		PreparedStatement pstmt = conn.prepareStatement(DATABASEMETADATA_TABLES_DDL_H2);
+		try {
+			pstmt.execute();
+		} finally {
+			pstmt.close();
+		}
+
+		pstmt = conn.prepareStatement(DATABASEMETADATA_COLUMNS_DDL_H2);
 		try {
 			pstmt.execute();
 		} finally {
--- src/test/java/blanco/test/h2/Simpleh2.java
+++ src/test/java/blanco/test/h2/Simpleh2.java
@@ -9,7 +9,6 @@ import java.sql.Statement;
 import junit.framework.TestCase;
 
 public class Simpleh2 extends TestCase {
-	//
 
 	public void test001() throws Exception {
 		Class.forName("org.h2.Driver");
@@ -20,6 +19,7 @@ public class Simpleh2 extends TestCase {
 		stmt.execute("CREATE TABLE SF_TABLES (TABLE_CAT VARCHAR, TABLE_SCHEM VARCHAR, TABLE_NAME VARCHAR" //
 				+ ", TABLE_TYPE VARCHAR DEFAULT 'TABLE', REMARKS VARCHAR, TYPE_CAT VARCHAR, TYPE_SCHEM VARCHAR, TYPE_NAME VARCHAR, SELF_REFERENCING_COL_NAME VARCHAR, REF_GENERATION VARCHAR DEFAULT 'USER')");
 		stmt.execute("insert into test values (1,'名前')");
+		conn.commit();
 
 		ResultSet rs = stmt.executeQuery("select * from test");
 
@@ -37,7 +37,7 @@ public class Simpleh2 extends TestCase {
 
 		stmt.close();
 
-		if (true) {
+		if (false) {
 			ResultSet rs2 = conn.getMetaData().getTables(null, null, null, null);
 			System.out.println("table");
 			for (; rs2.next();) {
@@ -47,10 +47,16 @@ public class Simpleh2 extends TestCase {
 			}
 		}
 
-		ResultSet rs3 = conn.getMetaData().getColumns(null, null, "test", null);
+		ResultSet rs3 = conn.getMetaData().getColumns(null, null, null, null);
 		for (; rs3.next();) {
-			System.out.println(rs3.getString(1));
-			System.out.println(rs3.getString(2));
+			// System.out.println(rs3.getString(1));
+			// System.out.println(rs3.getString(2));
+			for (int index = 1; index <= rs3.getMetaData().getColumnCount(); index++) {
+				System.out.print(", " + rs3.getMetaData().getColumnName(index) + " "
+						+ rs3.getMetaData().getColumnTypeName(index));
+			}
+			System.out.println();
+			break;
 		}
 
 		conn.close();
