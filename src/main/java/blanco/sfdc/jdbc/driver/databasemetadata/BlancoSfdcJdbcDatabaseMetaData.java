67931f9b9a7a42e21567aa8d9502d2c0c94ae7a4
Toshiki Iga <igapyon@gmail.com>
2017-09-09 09:40:33 +0900

    save

--- src/main/java/blanco/sfdc/jdbc/driver/databasemetadata/BlancoSfdcJdbcDatabaseMetaData.java
+++ src/main/java/blanco/sfdc/jdbc/driver/databasemetadata/BlancoSfdcJdbcDatabaseMetaData.java
@@ -46,6 +46,56 @@ public class BlancoSfdcJdbcDatabaseMetaData extends AbstractBlancoGenericJdbcDat
 		}
 	}
 
+	public static int soqlTypeName2SqlTypes(final String soqlTypeName) throws SQLException {
+		if ("VARCHAR".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.VARCHAR;
+		} else if ("string".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.VARCHAR;
+		} else if ("ID".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.VARCHAR;
+		} else if ("picklist".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.VARCHAR;
+		} else if ("multipicklist".compareToIgnoreCase(soqlTypeName) == 0) {
+			// TODO
+			return java.sql.Types.VARCHAR;
+		} else if ("anyType".compareToIgnoreCase(soqlTypeName) == 0) {
+			// TODO
+			return java.sql.Types.VARCHAR;
+		} else if ("reference".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.VARCHAR;
+		} else if ("phone".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.VARCHAR;
+		} else if ("url".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.VARCHAR;
+		} else if ("email".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.VARCHAR;
+		} else if ("currency".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.VARCHAR;
+		} else if ("textarea".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.LONGVARCHAR;
+		} else if ("address".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.LONGVARCHAR;
+		} else if ("_boolean".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.BOOLEAN;
+		} else if ("BOOLEAN".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.BOOLEAN;
+		} else if ("INTEGER".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.INTEGER;
+		} else if ("_int".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.INTEGER;
+		} else if ("_double".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.DOUBLE;
+		} else if ("DATE".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.DATE;
+		} else if ("DATETIME".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.TIMESTAMP;
+		} else if ("TIMESTAMP".compareToIgnoreCase(soqlTypeName) == 0) {
+			return java.sql.Types.TIMESTAMP;
+		}
+
+		throw new SQLException("Unsupported type:" + soqlTypeName);
+	}
+
 	public void fillCacheTableOfGetColumns(String catalog, String schema, String tableName, String columnNamePattern)
 			throws SQLException {
 		try {
@@ -104,10 +154,10 @@ public class BlancoSfdcJdbcDatabaseMetaData extends AbstractBlancoGenericJdbcDat
 					// "REMARKS"
 					pstmt.setString(rowNum++, field.getLabel());
 					// "SQL_DATA_TYPE" // reserved future
-					pstmt.setInt(rowNum++, 0);
+					pstmt.setInt(rowNum++, soqlTypeName2SqlTypes(field.getType().name()));
 					// "SQL_DATETIME_SUB"
 					pstmt.setInt(rowNum++, -1 /* FIXME */);
-					// rsmdRs.getInt("CHAR_OCTET_LENGTH")
+					// "CHAR_OCTET_LENGTH"
 					pstmt.setInt(rowNum++, field.getLength());
 
 					// ORDINAL_POSITION should not populate
